02d58b7425356e09ab139dbb50038b988503b698
Vladlina <92329114+Vladlina@users.noreply.github.com>
2025-01-29 17:27:32 +0100

    init commit and some libraries

---
+++ src/main.jsx
@@ -0,0 +1,19 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import "./index.css";
+import App from "./components/App/App.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <PersistGate loading={<Loader />} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </BrowserRouter>
+    </Provider>
+  </StrictMode>
+);
